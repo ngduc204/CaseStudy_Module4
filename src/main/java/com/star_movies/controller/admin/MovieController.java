c69d930a1edc9ef0b46f760329831159098b5afb
lxng <lxng@example.org>
2025-07-10 15:47:55 +0700

    add upload movie

--- src/main/java/com/star_movies/controller/admin/MovieController.java
+++ src/main/java/com/star_movies/controller/admin/MovieController.java
@@ -7,10 +7,14 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.util.List;
 
 import com.star_movies.model.Movie;
+import com.star_movies.service.HandleUploadService;
 import com.star_movies.service.MovieService;
 
 @Controller
@@ -18,9 +22,11 @@ import com.star_movies.service.MovieService;
 public class MovieController {
 
     private final MovieService movieService;
+    private final HandleUploadService handleUploadService;
 
-    public MovieController(MovieService movieService) {
+    public MovieController(MovieService movieService, HandleUploadService handleUploadService) {
         this.movieService = movieService;
+        this.handleUploadService = handleUploadService;
     }
 
     @GetMapping
@@ -48,8 +54,13 @@ public class MovieController {
     }
 
     @PostMapping("/save")
-    public String saveMovie(@ModelAttribute("movie") Movie movie) {
-
+    public String saveMovie(@ModelAttribute("movie") Movie movie, @RequestParam("movieFile") MultipartFile movieFile) {
+        try {
+            String moviePath = handleUploadService.handleUpload(movieFile, "movies");
+            movie.setMovie(moviePath);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         movieService.save(movie);
         return "redirect:/admin/movies";
     }
